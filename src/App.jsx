ae3f51c8d2936c842fafb5257d55fd696b3be6b5
lashasuxa <lashasukhashvili1995@gmail.com>
2023-04-30 02:21:59 -0400

    markup edit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,11 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Sun from "./assets/icon-sun.svg"
+import Moon from "./assets/icon-moon.svg"
+import BcgDeskLight from "./assets/bg-desktop-light.jpg"
+import BcgDeskDark from "./assets/bg-desktop-dark.jpg"
+
 
 function App() {
   const[todos,setTodos]=useState()
@@ -12,7 +17,7 @@ function App() {
         <div className="whole_card">
             <div className="header">
                 <h1>TODO</h1>
-                <button className="theme"><img src="./assets/icon-sun.svg" alt="sum" /></button>
+                <button className="theme"><img src={Sun} alt="Sun" /></button>
             </div>
             <div className="todoinputContainer">
               <button></button>
